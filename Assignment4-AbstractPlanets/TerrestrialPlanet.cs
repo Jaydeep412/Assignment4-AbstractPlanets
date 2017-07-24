1904855e2af8ba91ff0bd26f946cd7f72bdebf94
Jaydeep <jlakhawani@gmail.com>
2017-07-24 18:53:06 -0400

    Created the TerrestrialPlanet class that extends the Planet abstract class.

---
+++ Assignment4-AbstractPlanets/TerrestrialPlanet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+* Name: Jaydeep Lakhwani
+* Date: July 24,2017
+* Description: This is the TerrestrialPlanet Class that extends the Planet abstract class.
+* Version: 0.1 - Created the TerrestrialPlanet class.
+*/
+
+namespace Assignment4_AbstractPlanets
+{
+    class TerrestrialPlanet : Planet
+    {
+        public TerrestrialPlanet(string name, double diameter, double mass) 
+            : base(name, diameter, mass)
+        {
+        }
+    }
+}
