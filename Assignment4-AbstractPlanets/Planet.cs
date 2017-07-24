1a8bb809454a7496c5ad7cea5e99f0ed37188913
Jaydeep <jlakhawani@gmail.com>
2017-07-24 16:08:13 -0400

    Created the Planet abstract class - half

---
+++ Assignment4-AbstractPlanets/Planet.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+* Name: Jaydeep Lakhwani
+* Date: July 24,2017
+* Description: This is the Abstract Planet Class
+* Version: 0.1 - Created the Abstract Planet Class
+*/
+
+namespace Assignment4_AbstractPlanets
+{
+    /// <summary>
+    /// This is Planet abstract class
+    /// </summary>
+    public abstract class Planet
+    {
+
+        // PRIVATE INSTANCE VARIABLES (FIELDS)
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+
+        // PUBLIC PROPERTIES
+
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+        }
+
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+        }
+
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+
+            }
+        }
+
+        
+
+
+    }
+}
