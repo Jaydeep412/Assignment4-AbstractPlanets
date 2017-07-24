a5d3aafccfd2681eba1622ae2af31030c0bd4885
Jaydeep <jlakhawani@gmail.com>
2017-07-24 19:29:07 -0400

    Created IHasMoons interface.

---
+++ Assignment4-AbstractPlanets/IHasMoons.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Name: Jaydeep Lakhwani
+* Date: July 24,2017
+* Description: This is the IHasMoons interface.
+* Version: 0.1 - Created IHasMoons interface.
+*/
+
+namespace Assignment4_AbstractPlanets
+{
+    interface IHasMoons
+    {
+
+    }
+}
