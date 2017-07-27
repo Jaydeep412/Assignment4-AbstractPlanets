09645bbcda38d13beb927b30dd00b243e039fdb6
Jaydeep <jlakhawani@gmail.com>
2017-07-27 19:56:31 -0400

    Added comments to driver class and tested driver class.

--- Assignment4-AbstractPlanets/Program.cs
+++ Assignment4-AbstractPlanets/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 * Name: Jaydeep Lakhwani
 * Date: July 24,2017
 * Description: This is the driver program class.
-* Version: 0.2 -Created the terrestrialPlanet object
+* Version: 0.3 -Added comments and tested.
 */
 
 namespace Assignment4_AbstractPlanets
@@ -17,16 +17,20 @@ namespace Assignment4_AbstractPlanets
     {
         static void Main(string[] args)
         {
+            //Created GiantPlanet object
             GiantPlanet giantPlanet = new GiantPlanet("Jupitor",140000,Convert.ToDouble(1.898 * Math.Pow(10,27) ),"Gas");
 
-            
+            //Printed GiantPlanet object to the console
             Console.WriteLine(giantPlanet.ToString());
 
+            //Created terrestrialPlanet object
             TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Earth",13000, Convert.ToDouble(5.972 * Math.Pow(10, 24)),true);
 
-
+            //Printed terrestrialPlanet object to the console
             Console.WriteLine(terrestrialPlanet.ToString());
 
+
+            //Added read key function
             Console.ReadKey();
         }
     }
