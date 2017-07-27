49c203e64e60474cea7c7971c52801e2dd57f210
Jaydeep <jlakhawani@gmail.com>
2017-07-27 19:52:26 -0400

    TerrestrialPlanet object in driver class

--- Assignment4-AbstractPlanets/Program.cs
+++ Assignment4-AbstractPlanets/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 * Name: Jaydeep Lakhwani
 * Date: July 24,2017
 * Description: This is the driver program class.
-* Version: 0.1 -Created the giantPlanet object
+* Version: 0.2 -Created the terrestrialPlanet object
 */
 
 namespace Assignment4_AbstractPlanets
@@ -17,11 +17,17 @@ namespace Assignment4_AbstractPlanets
     {
         static void Main(string[] args)
         {
-            GiantPlanet giantPlanet = new GiantPlanet("Jupitor",140000,317.8,"Gas");
-
-            Console.WriteLine(giantPlanet.ToString());
+            GiantPlanet giantPlanet = new GiantPlanet("Jupitor",140000,Convert.ToDouble(1.898 * Math.Pow(10,27) ),"Gas");
 
             
+            Console.WriteLine(giantPlanet.ToString());
+
+            TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Earth",13000, Convert.ToDouble(5.972 * Math.Pow(10, 24)),true);
+
+
+            Console.WriteLine(terrestrialPlanet.ToString());
+
+            Console.ReadKey();
         }
     }
 }
