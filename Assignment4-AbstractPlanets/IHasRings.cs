57d6eeaf78374f0afb5b426cf5c0f1c09f3b88d7
Jaydeep <jlakhawani@gmail.com>
2017-07-27 13:28:15 -0400

    Created IHasRings interface

---
+++ Assignment4-AbstractPlanets/IHasRings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Name: Jaydeep Lakhwani
+* Date: July 27,2017
+* Description: This is the IHasRings interface.
+* Version: 0.1 - Created IHasRings interface.
+
+*/
+namespace Assignment4_AbstractPlanets
+{
+    interface IHasRings
+    {
+        //PUBLIC PROPERTIES
+        bool HasRings { get; set; }
+    }
+}
